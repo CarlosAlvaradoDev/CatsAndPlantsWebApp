565000e9b79b243c87922ecfa5ce0117f1009e47
Carlos Alvarado <wikiwakawakawee@gmail.com>
2021-04-11 11:09:13 -0600

    Add project files.

---
+++ CatsAndPlants/Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatsAndPlants.Models;
+
+namespace CatsAndPlants.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Customer form)
+        {
+            if (ModelState.IsValid)
+            {
+               return RedirectToAction("Details", form);
+            }
+            return View();
+        }
+
+        public IActionResult Details(Customer form)
+        {
+            return View(form);
+        }
+
+        [HttpGet]
+        public IActionResult EditDetails(int id)
+        {
+            // Faking out going to the database and getting "id" details.
+            Customer charlies = new Customer();
+            charlies.CustomerId = id;
+            charlies.FirstName = "Charlies";
+            charlies.LastName = "Avocado";
+            charlies.Email = "c@c.com";
+            charlies.Phone = "123";
+            charlies.ShipAddress = "123 North";
+            charlies.ShipCity = "Phoenix";
+            charlies.ShipState = "AZ";
+            charlies.ShipPostal = 52234;
+            charlies.BillAddress = "123 North";
+            charlies.BillCity = "Phoenix";
+            charlies.BillState = "AZ";
+            charlies.BillPostal = 52234;
+            return View(charlies);
+        }
+
+        [HttpPost]
+        public IActionResult EditDetails(Customer updatedDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Details", updatedDetails);
+            }
+            return View(updatedDetails);
+        }
+    }
+}
